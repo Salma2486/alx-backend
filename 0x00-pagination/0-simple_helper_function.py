e88e1e159397ab3e018eb346a5d07fbb05845a88
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-27 14:55:36 +0000

    t

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,6 @@
 from typing import Tuple
 
 
-def index_range(page, page_size)-> Tuple[int, int]:
+def index_range(page, page_size) -> Tuple[int, int]:
     """index range"""
     return (page * page_size - page_size, page * page_size)
