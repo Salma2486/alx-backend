2e83decf493165c28849676911e7b4dd4dc7e5e4
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-27 14:54:07 +0000

    t

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task0"""
+from typing import Tuple
+
+def index_range(page, page_size)-> Tuple[int, int]:
+    """index range"""
+    return (page * page_size - page_size, page * page_size)
