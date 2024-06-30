3e45763880d6be60a0313bfc9c74c22459e71721
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-30 16:58:55 +0000

    r

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" LIFOCache module
+"""
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache is a caching system that inherits from BaseCaching
+        It uses a Last-In, First-Out (LIFO) cache replacement policy
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Remove the most recently added item (LIFO)
+            last_key = list(self.cache_data.keys())[-1]
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
