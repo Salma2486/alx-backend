fdcd6aa2984a9b0adf3c18f67d6307171f3273d3
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-07-03 11:13:58 +0000

    h

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+"""tdyh jtdyj tdy j"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """tdyh jtdyj tdy j"""
     return render_template('index.html')
 
 if __name__ == '__main__':
